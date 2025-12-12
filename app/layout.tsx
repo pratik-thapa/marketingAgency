d874cb7122e36f0b019f939a83b1ee661d8fcb78
pratik-thapa <prateekthapa02@gmail.com>
2025-12-12 16:32:32 +0000

    Initial Next.js installation

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Marketing Agency", // I updated this for you
+  description: "Professional Growth & Strategy", // I updated this for you
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true} /* <-- This fixes the extension error */
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
